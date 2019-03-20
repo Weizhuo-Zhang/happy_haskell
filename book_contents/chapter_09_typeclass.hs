1472cb74e522416ae68c9237ac4dc6b2d7458f76
WeizhuoZhang <wzhuo.zhang@gmail.com>
2019-03-20 21:57:55 +1100

    learned how to define a data structure like class

--- book_contents/chapter_09_typeclass.hs
+++ book_contents/chapter_09_typeclass.hs
@@ -1,3 +1,12 @@
+module Shapes
+( Point(..)
+, Shape(..)
+, surface
+, nudge
+, baseCircle
+, baseRect
+) where
+
 -- data Shape = Circle Float Float Float | Rectangle Float Float Float Float deriving (Show)
 
 -- surface :: Shape -> Float
@@ -41,6 +50,97 @@ baseCircle r = Circle (Point 0 0) r
 
 baseRect :: Float -> Float -> Shape
 baseRect width height = Rectangle (Point 0 0) (Point width height)
+
+
+-- Record Syntax
+--
+
+-- First_name, Last name, Age...
+-- data Person = Person String String Int Float String String deriving (Show)
+
+-- ghci> let guy = Person "Buddy" "Finklestein" 43 184.2 "526-2928" "Chocolate"
+-- ghci> guy
+-- Person "Buddy" "Finklestein" 43 184.2 "526-2928" "Chocolate"
+
+
+-- firstName :: Person -> String
+-- firstName (Person firstname _ _ _ _ _) = firstname
+--
+-- lastName :: Person -> String
+-- lastName (Person _ lastName _ _ _ _) = lastName
+--
+-- age :: Person -> Int
+-- age (Person _ _ age _ _ _) = age
+--
+-- height :: Person -> Float
+-- height (Person _ _ _ height _ _) = height
+--
+-- phoneNumber :: Person -> String
+-- phoneNumber (Person _ _ _ _ number _) = number
+--
+-- flavor :: Person -> String
+-- flavor (Person _ _ _ _ _ flavor) = flavor
+
+-- ghci> firstName guy
+-- "Buddy"
+-- ghci> height guy
+-- 184.2
+-- ghci> flavor guy
+-- "Chocolate"
+
+
+
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , flavor :: String
+                     } deriving (Show)
+
+-- ghci> :t flavor
+-- flavor :: Person -> String
+-- ghci> :t firstName
+-- firstName :: Person -> String
+
+-- data Car = Car String String Int deriving (Show)
+
+-- ghci> Car "Ford" "Mustang" 1967
+-- Car "Ford" "Mustang" 1967
+
+data Car = Car {company :: String, model :: String, year :: Int} deriving (Show)
+
+-- ghci> Car {company="Ford", model="Mustang", year=1967}
+-- Car {company = "Ford", model = "Mustang", year = 1967}
+
+
+-- Type parameters
+--
+-- ghci> Just "Haha"
+-- Just "Haha"
+-- ghci> Just 84
+-- Just 84
+-- ghci> :t Just "Haha"
+-- Just "Haha" :: Maybe [Char]
+-- ghci> :t Just 84
+-- Just 84 :: Num a => Maybe a
+-- ghci> :t Nothing
+-- Nothing :: Maybe a
+-- ghci> Just 10 :: Maybe Double
+
+
+tellCar :: Car -> String
+tellCar (Car {company=c, model=m, year=y}) = "This " ++ c ++ " " ++ m ++ " war made in " ++ show y
+
+-- ghci> let stang = Car {company="Ford", model="Mustang", year=1967}
+-- ghci> tellCar stang
+-- "This Ford Mustang war made in 1967"
+
+
+
+
+
+
 
 
 

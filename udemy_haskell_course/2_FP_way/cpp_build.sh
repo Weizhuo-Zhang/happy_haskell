190b33360cb65fffb5ecde0033afe59d1812641e
WeizhuoZhang <wzhuo.zhang@gmail.com>
2019-04-06 13:14:32 +1100

    finish 6_FP_WAY

--- udemy_haskell_course/2_FP_way/cpp_build.sh
+++ udemy_haskell_course/2_FP_way/cpp_build.sh
@@ -1,4 +1,7 @@
 #g++ Traversal.cpp -o TraversalCPP.out -std=c++11
 
-g++ Reduction.cpp -o ReductionCPP.out -std=c++11
+#g++ Reduction.cpp -o ReductionCPP.out -std=c++11
 
+#g++ Filter.cpp -o FilterCPP.out -std=c++11
+
+g++ Composition.cpp -o CompositionCPP.out -std=c++11

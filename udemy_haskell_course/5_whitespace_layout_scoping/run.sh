1d39e810d80e1ec9ae6205c158a9c7a14da4ee41
WeizhuoZhang <wzhuo.zhang@gmail.com>
2019-04-06 19:13:20 +1100

    finish 13 type

---
+++ udemy_haskell_course/5_whitespace_layout_scoping/run.sh
@@ -0,0 +1,3 @@
+# -fno-code tells GHC to compile the code not to generate any output files
+
+stack ghc -- -fno-code Program9.hs
---
+++ udemy_haskell_course/6_type/Overloading.cpp
@@ -0,0 +1,18 @@
+#include <string>
+using namespace std;
+
+void func(int x) { }
+
+void func(const string& x) { }
+
+void func(double x) { }
+
+void func(int x, int y, int z) { }
+
+void example()
+{
+    func(5);
+    func("hello");
+    func(5.0);
+    func(5, 6, 7);
+}

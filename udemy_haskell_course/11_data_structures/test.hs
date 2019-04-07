ee2f24e9c24b5fb4ec8911169db1a715ac8a0057
WeizhuoZhang <wzhuo.zhang@gmail.com>
2019-04-07 14:18:23 +1000

    finish 27 patter match

--- udemy_haskell_course/11_data_structures/test.hs
+++ udemy_haskell_course/11_data_structures/test.hs
@@ -5,3 +5,16 @@ data Expression = Number Int
                 | Add Expression Expression
                 | Substract Expression Expression
                 deriving (Eq, Ord, Show)
+
+calculate :: Expression -> Int
+calculate (Number x) = x
+calculate (Add x y)  = (calculate x) + (calculate y)
+calculate (Substract x y)  = (calculate x) - (calculate y)
+
+newHead :: [a] -> a
+newHead [] = error "empty list"
+newHead (x:xs) = x
+
+newHead :: [a] -> [a]
+newTail [] = error "empty list"
+newTail (x:xs) = xs

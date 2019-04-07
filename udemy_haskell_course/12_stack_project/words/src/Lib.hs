59776e086b9517db424d3171948d3e04b4a459db
WeizhuoZhang <wzhuo.zhang@gmail.com>
2019-04-07 15:49:16 +1000

    finish 31 searching in all directions

--- udemy_haskell_course/12_stack_project/words/src/Lib.hs
+++ udemy_haskell_course/12_stack_project/words/src/Lib.hs
@@ -8,9 +8,10 @@ module Lib
     , findWord
     , findWords
     , findWordInLine
+    , skew
     ) where
 
-import Data.List (isInfixOf)
+import Data.List (isInfixOf, transpose)
 import Data.Maybe (catMaybes)
 
 type Grid = [String]
@@ -27,9 +28,27 @@ outputGrid grid = putStrLn (formatGrid grid)
 formatGrid :: Grid -> String
 formatGrid = unlines
 
+getLines :: Grid -> [String]
+getLines grid =
+    let horizontal = grid
+        vertical = transpose grid
+        diagonal1 = diagonalize horizontal
+        diagonal2 = diagonalize (map reverse horizontal)
+        lines = horizontal ++ vertical ++ diagonal1 ++ diagonal2
+    in lines ++ (map reverse lines)
+
+diagonalize :: Grid -> Grid
+diagonalize = transpose . skew
+-- diagonalize grid = transpose (skew grid)
+
+skew :: Grid -> Grid
+skew [] = []
+skew (l:ls) = l : skew (map indent ls)
+    where indent line = '_' : line
+
 findWord :: Grid -> String -> Maybe String
 findWord grid word =
-    let lines = grid ++ (map reverse grid)
+    let lines = getLines grid
         found = or $ map (findWordInLine word) lines
     in if found then Just word else Nothing
 
